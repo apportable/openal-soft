0965fa43d51589dc211d4d2232d78dd8c586c67f
Marc Salem <salem@apportable.com>
2012-01-03 14:22:38 -0800

    Add alcSuspend and alcResume

--- jni/OpenAL/Alc/android.c
+++ jni/OpenAL/Alc/android.c
@@ -7,7 +7,19 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
 	BackendFuncs func_list;
 	alc_audiotrack_init(&func_list);
 	apportableOpenALFuncs.alc_android_set_java_vm(vm);
-    return JNI_VERSION_1_4;
+	return JNI_VERSION_1_4;
+}
+
+ALC_API void ALC_APIENTRY alcSuspend(void) {
+	if (apportableOpenALFuncs.alc_android_suspend) {
+		apportableOpenALFuncs.alc_android_suspend();
+	}
+}
+
+ALC_API void ALC_APIENTRY alcResume(void) {
+	if (apportableOpenALFuncs.alc_android_resume) {
+		apportableOpenALFuncs.alc_android_resume();
+	}
 }
 
 #endif
