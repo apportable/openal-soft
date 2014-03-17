fe512490e262403aeede1646231e591f9cd69f11
Philippe Hausler <philippe@apportable.com>
2014-03-17 14:45:44 -0700

    remove JNICALL declaration since it is no longer needed

--- jni/OpenAL/Alc/android.c
+++ jni/OpenAL/Alc/android.c
@@ -17,7 +17,7 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
 	return JNI_VERSION_1_4;
 }
 
-void JNICALL JNI_OnUnload (JavaVM *vm, void *reserved)
+void JNI_OnUnload (JavaVM *vm, void *reserved)
 {
 	if (apportableOpenALFuncs.alc_android_set_java_vm) {
 		apportableOpenALFuncs.alc_android_set_java_vm(NULL);
